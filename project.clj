d20ab117a79de5b9e06b9a32f9024d52edd8902d
Gary Fredericks <fredericksgary@gmail.com>
2013-11-05 07:35:40 -0600

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject com.gfredericks/git-git "0.1.0-SNAPSHOT"
   :description "Managing your swarm of git repositories."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/fredericksgary/git-git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[me.raynes/conch "0.5.2"]
                  [me.raynes/fs "1.4.5"]
                  [org.clojure/clojure "1.5.1"]
                  [robert/hooke "1.3.0"]]
+
+  ;; building
+  :aot :all
   :main com.gfredericks.git-git
-  :aot :all)
+  :uberjar-name "git-git.jar")
